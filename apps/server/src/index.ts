044bb315359f925666307f66a96b52d01185a5ac
Soorya U <sooryau7@gmail.com>
2024-01-01 18:31:13 +0530

    Init Server Side Sockets

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,12 +1,19 @@
 import http from "http";
+import SocketService from "./services/socket";
 
 async function init() {
+  const sockerService = new SocketService();
+
   const httpServer = http.createServer();
   const PORT = process.env.PORT ?? 6000;
+
+  sockerService.io.attach(httpServer);
 
   httpServer.listen(PORT, () =>
     console.log(`Server started at http://localhost:${PORT}`)
   );
+
+  sockerService.initListeners();
 }
 
 init();
---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,28 @@
+import { Server } from "socket.io";
+
+class SocketService {
+  private _io: Server;
+
+  constructor() {
+    console.log("Init Socket Server");
+    this._io = new Server();
+  }
+
+  public initListeners() {
+    console.log("Init Socket Listeners");
+    const io = this._io;
+    io.on("connect", (socket) => {
+      console.log("New Socket Connected:", socket.id);
+
+      socket.on("event:message", async ({ message }: { message: String }) => {
+        console.log("New Message Recieved:", message);
+      });
+    });
+  }
+
+  get io() {
+    return this._io;
+  }
+}
+
+export default SocketService;
