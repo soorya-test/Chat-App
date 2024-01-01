cd9c42e4473fc3ca73fa849a534a392ef1393646
Soorya U <sooryau7@gmail.com>
2024-01-01 19:44:35 +0530

    Sending Message from Client Side

--- apps/web/app/context/socketProvider.tsx
+++ apps/web/app/context/socketProvider.tsx
@@ -6,8 +6,9 @@ import {
   useCallback,
   useContext,
   useEffect,
+  useState,
 } from "react";
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
 interface SocketProviderProps {
   children: ReactNode;
@@ -26,15 +27,25 @@ export const useSocket = () => {
 };
 
 export const SocketProvider: FC<SocketProviderProps> = ({ children }) => {
-  const sendMessage: ISocketContext["sendMessage"] = useCallback((msg) => {
-    console.log("Send Message", msg);
-  }, []);
+  const [socket, setSocket] = useState<Socket>();
+
+  const sendMessage: ISocketContext["sendMessage"] = useCallback(
+    (msg) => {
+      console.log("Send Message", msg);
+      if (socket) {
+        socket.emit("event:message", { message: msg });
+      }
+    },
+    [socket]
+  );
 
   useEffect(() => {
     const _socket = io("http://localhost:8000");
+    setSocket(_socket);
 
     return () => {
       _socket.disconnect();
+      setSocket(undefined);
     };
   }, []);
 
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,39 @@
-'use Client'
+"use client";
+
+import { useState } from "react";
+import { useSocket } from "./context/socketProvider";
 
 export default function Page() {
+  const { sendMessage } = useSocket();
+  const [message, setMessage] = useState("");
+
   return (
-    <h1 className="text-3xl font-bold text-[red]">Home Page</h1>
-  )
+    <>
+      <div>
+        <h1 className="text-3xl font-bold text-[#fff] text-center">
+          All Messages will Appear Here
+        </h1>
+      </div>
+      <form className="w-[100vw] fixed bottom-10 flex justify-center items-center gap-[1rem]">
+        <input
+          className="border-[2px] border-[#fff] rounded-[15px] p-[0.5rem] bg-[#000] text-[#fff]"
+          type="text"
+          placeholder="Enter a Message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            sendMessage(message);
+            setMessage("");
+          }}
+          className="text-[#fff] border-[2px] p-[0.5rem] rounded-[15px]"
+          type="submit"
+        >
+          Send
+        </button>
+      </form>
+    </>
+  );
 }
